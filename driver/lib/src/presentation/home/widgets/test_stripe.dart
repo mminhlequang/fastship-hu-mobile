dc735e8b54907500f79decb10ce695b1679e9d97
Minh Le Quang <>
2025-03-02 11:06:52 +0700

    add test socket

---
+++ driver/lib/src/presentation/home/widgets/test_stripe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class WidgetTestStripe extends StatefulWidget {
+  const WidgetTestStripe({super.key});
+
+  @override
+  State<WidgetTestStripe> createState() => _WidgetTestStripeState();
+}
+
+class _WidgetTestStripeState extends State<WidgetTestStripe> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
